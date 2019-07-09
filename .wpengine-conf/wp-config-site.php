3db13d5a8734cd18e00ff5531982db549e5241fb
michellemandel <michelle.mandel@wpengine.com>
2019-07-09 11:08:19 -0500

    initial commit

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,6 @@
+<?php
+// GENERATED AUTOMATICALLY ON Tue, 09 Jul 2019 16:07:46 +0000
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "mmandelstaging.wpengine.com";
+$wpengine_primary_original_ip_address = "104.198.137.50";
+			
